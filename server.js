4c8d3b061d80dee8dfd55e9560fce12312f45aa3
Liam Allen <liamallen343@gmail.com>
2023-06-26 15:09:22 -0500

    Another

--- server.js
+++ server.js
@@ -41,11 +41,11 @@ mongoose.connect("mongodb+srv://liamallen343:liamallen34@moonlitecluster.fhjc5xd
           `<script>
             var serverData = ${jsonData};
             console.log(serverData);
-            document.getElementById("slide1").style.backgroundImage = "url(serverData[0].movie1)";
-            document.getElementById("slide2").style.backgroundImage = "url(serverData[0].movie2)";
-            document.getElementById("slide3").style.backgroundImage = "url(serverData[0].movie3)";
-            document.getElementById("slide4").style.backgroundImage = "url(serverData[0].movie4)";
-            document.getElementById("announceImg").style.backgroundImage = "url(serverData[0].announceImg)";
+            document.getElementById("slide1").style.backgroundImage = url(serverData[0].movie1);
+            document.getElementById("slide2").style.backgroundImage = url(serverData[0].movie2);
+            document.getElementById("slide3").style.backgroundImage = url(serverData[0].movie3);
+            document.getElementById("slide4").style.backgroundImage = url(serverData[0].movie4);
+            document.getElementById("announceImg").style.backgroundImage = url(serverData[0].announceImg);
             document.getElementById("announceHead").innerHTML = serverData[0].announceHead;
             document.getElementById("announceText").innerHTML = serverData[0].announceText;
           </script>`
