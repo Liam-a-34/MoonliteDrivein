f44912321e9d9dead7fb92316826c3bfc90484fc
Liam Allen <liamallen343@gmail.com>
2023-06-26 15:57:20 -0500

    Added path requirement to the top

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
 const app = express();
+const path = require("path")
 
 const moonliteSchema = new mongoose.Schema({
   movie1: String,
